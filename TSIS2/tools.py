bfdcdea539bdec13ed8df9ef0bd82c3bb4059808
NursezimShorman <n_shorman@kbtu.kz>
2026-05-04 01:25:05 +0500

    Update tools.py

--- TSIS2/tools.py
+++ TSIS2/tools.py
@@ -1,9 +1,14 @@
 import pygame
 
-# -------------------- FLOOD FILL --------------------
 def flood_fill(surface, x, y, new_color):
     width, height = surface.get_size()
-    target_color = surface.get_at((x, y))
+
+    # bounds check
+    if x < 0 or x >= width or y < 0 or y >= height:
+        return
+
+    target_color = surface.get_at((x, y))[:3]
+    new_color = new_color[:3]
 
     if target_color == new_color:
         return
@@ -13,14 +18,15 @@ def flood_fill(surface, x, y, new_color):
     while stack:
         x, y = stack.pop()
 
-        if surface.get_at((x, y)) == target_color:
+        if x < 0 or x >= width or y < 0 or y >= height:
+            continue
+
+        current_color = surface.get_at((x, y))[:3]
+
+        if current_color == target_color:
             surface.set_at((x, y), new_color)
 
-            if x > 0:
-                stack.append((x - 1, y))
-            if x < width - 1:
-                stack.append((x + 1, y))
-            if y > 0:
-                stack.append((x, y - 1))
-            if y < height - 1:
-                stack.append((x, y + 1))
+            stack.append((x + 1, y))
+            stack.append((x - 1, y))
+            stack.append((x, y + 1))
+            stack.append((x, y - 1))
