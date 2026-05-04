da046f514a32a8aa61c8fbbf684ea5c5816a9b30
NursezimShorman <n_shorman@kbtu.kz>
2026-05-04 14:10:35 +0500

    Refactor database connection to use config parser

--- TSIS4/config.py
+++ TSIS4/config.py
@@ -1,9 +1,16 @@
-import psycopg2
+from configparser import ConfigParser
 
-def connect():
-    return psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="32mYhDNA"
-    )
+
+def load_config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception("Section {0} not found in the {1} file".format(section, filename))
+
+    return config
